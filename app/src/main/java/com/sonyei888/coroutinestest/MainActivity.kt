edb4bfb01582f9cbc097fa3cd37b50838cb068f4
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-03 21:48:21 -0500

    Primer ejemplo de corrutinas

--- app/src/main/java/com/sonyei888/coroutinestest/MainActivity.kt
+++ app/src/main/java/com/sonyei888/coroutinestest/MainActivity.kt
@@ -9,10 +9,19 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sonyei888.coroutinestest.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
 
     private lateinit var binding: ActivityMainBinding
+
+    override val coroutineContext: CoroutineContext
+        get() = TODO("Not yet implemented")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +30,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.submit.setOnClickListener {
-            val success = validateLogin(binding.userName.text.toString(), binding.Password.text.toString())
-            toast(if (success) "Success" else "Failure")
+            GlobalScope.launch(Dispatchers.Main) {
+                val success = withContext(Dispatchers.IO) {
+                    validateLogin(
+                        binding.userName.text.toString(),
+                        binding.Password.text.toString()
+                    )
+                }
+                toast(if (success) "Success" else "Failure")
+            }
         }
     }
 
     private fun validateLogin(user: String, pass: String): Boolean {
+        Thread.sleep(2000)
         return user.isNotEmpty() && pass.isNotEmpty()
     }
+
+
 }
 
 private fun Context.toast(message: String) {
